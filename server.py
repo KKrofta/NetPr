3fbe8b88e64ba42f2686078541e1ca908c55e330
Kevin Krofta <kkrofta@techfak.uni-bielefeld.de>
2018-06-19 11:16:37 +0200

    started adding the client registration and added comments for working with json in python

--- server.py
+++ server.py
@@ -1,1 +1,29 @@
+import socket
+import json
 
+clients = []
+
+def main():
+	host = "localhost"
+	port = 5000
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+	s.bind((host, port))
+	s.listen(socket.SOMAXCONN)
+
+def registerClient():
+	try:
+		while 1:
+			inSocket, addr = s.accept()
+			info = s.recv(500)
+			info.decode("utf-8")
+			
+	finally
+		s.close()
+
+if __name__ == "__main__":
+    main()
+
+#json.dump(data, write_file)    writes to json converted data in write_file
+#json.dumps(data)   returns data as json-string
+#json.loads(json_string)   returns data as in python format (dict for json objects) depending on json_string
+#json.load(read_file)   see loads
